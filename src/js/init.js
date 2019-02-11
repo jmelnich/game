9e8effb95eb0d493f63facf9ec2547309faab0ad
Julia <julyettka@gmail.com>
2019-02-11 13:51:47 +0200

    fix start position

--- src/js/init.js
+++ src/js/init.js
@@ -13,9 +13,9 @@ function openField() {
     const randomPosition = arrPosition[Math.floor(Math.random() * arrPosition.length)];
     const randomSpeed = arrSpeed[Math.floor(Math.random() * arrSpeed.length)];
     move (block, randomSpeed, randomPosition);
-    // setTimeout(() => {
-    //   document.getElementsByClassName('block')[0].remove();
-    // }, 4000);
+    setTimeout(() => {
+      document.getElementsByClassName('blockContainer')[0].remove();
+    }, 4000);
   }, 1000);
 }
 start.addEventListener('click', openField);
