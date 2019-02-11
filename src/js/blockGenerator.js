dc4b8ee287ee42a091c4ffb07b2381750164bdbf
Julia <julyettka@gmail.com>
2019-02-11 14:07:10 +0200

    block gener changes

--- src/js/blockGenerator.js
+++ src/js/blockGenerator.js
@@ -7,13 +7,29 @@ export function generateBlock(mainContainerHeight=700) {
 	const BLOCK_HEIGHT = mainContainerHeight * 0.2;
 
 	const randomHeight = Math.round(Math.random() * 100 * mainContainerHeight / 100 * 0.8);
+	const lowPoint = randomHeight + BLOCK_HEIGHT;
 
 	blockContainer.classList.add(BLOCK_CONTAINER_CLASS);
-	blockContainer.innerHTML = `<div class="wrapper"><div class="${INNER_BLOCK_CLASS}" 
-style="height: ${BLOCK_HEIGHT + 'px'}; top: 
+	blockContainer.innerHTML = `<div class="wrapper"><div class="${INNER_BLOCK_CLASS}" style="height: ${BLOCK_HEIGHT + 'px'}; top: 
 ${randomHeight + 'px'}"></div></div>`;
 	mainContainer.appendChild(blockContainer);
 
 	return blockContainer;
 };
 
+
+// t INNER_BLOCK_TOP_CLASS = 'innerBlockTop';
+// const INNER_BLOCK_BOTTOM_CLASS = 'innerBlockBottom';
+// const BLOCK_CONTAINER_CLASS = 'blockContainer';
+//
+// const randomHeight = Math.round(Math.random() * 100 * mainContainerHeight / 100 * 0.8);
+// const gapWay = randomHeight * 0.2;
+//
+// blockContainer.classList.add(BLOCK_CONTAINER_CLASS);
+//
+// const topBlockHeight = randomHeight;
+// const bottomBlockHeight = mainContainerHeight - randomHeight - gapWay;
+//
+// blockContainer.innerHTML = `<div class="wrapper"><div class="${INNER_BLOCK_TOP_CLASS}" style="height: ${topBlockHeight + 'px'}"></div><div class="${INNER_BLOCK_BOTTOM_CLASS}" style="height: ${bottomBlockHeight + 'px'}; margin-top: ${gapWay + 'px'}"></div></div>`;
+// mainContainer.appendChild(blockContainer);
+//
